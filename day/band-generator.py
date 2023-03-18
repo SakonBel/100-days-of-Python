8482bd7c45b6117ed0d9569a0ba3855fba1a074a
Sakon Plangklang <iamsabell@gmail.com>
2023-03-18 23:49:08 +0700

    Create tip calculator function.

--- day/band-generator.py
+++
@@ -1,6 +0,0 @@
-print("Let me suggest you the name of your music band.")
-
-country = input("What country are your birthplace? : ")
-pet_name = input("What is the name of your pet? : ")
-
-print("Your band name is " + country + " " + pet_name + "!")
---
+++ day_001/band-generator.py
@@ -0,0 +1,7 @@
+#Pring the needed messages.
+print("Let me suggest you the name of your music band.\n")
+
+country = input("What country are your birthplace? :\n")
+pet_name = input("What is the name of your pet? :\n")
+
+print("\nYour band name is " + country + " " + pet_name + "!")
