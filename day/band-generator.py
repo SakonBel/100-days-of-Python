4ae3c3f2c379f085e604c31c4358ec9f9aa3741d
Sakon Plangklang <iamsabell@gmail.com>
2023-03-18 21:39:54 +0700

    Day 1: Created band generator app

---
+++ day/band-generator.py
@@ -0,0 +1,6 @@
+print("Let me suggest you the name of your music band.")
+
+country = input("What country are your birthplace? : ")
+pet_name = input("What is the name of your pet? : ")
+
+print("Your band name is " + country + " " + pet_name + "!")
