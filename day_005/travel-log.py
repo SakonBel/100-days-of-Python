760f0b5c01c938d7330789cad95f62920c7a6ccc
Sakon Plangklang <iamsabell@gmail.com>
2023-03-23 15:27:10 +0700

    Create travel-log program

--- day_005/travel-log.py
+++ day_005/travel-log.py
@@ -11,3 +11,16 @@ travel_log = [
   },
 ]
 
+def add_new_country(country, visits, cities):
+  travel_log.append(
+    {
+      "country": country,
+      "visits": visits,
+      "cities": cities,
+    }
+  )
+
+
+
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+print(travel_log)
