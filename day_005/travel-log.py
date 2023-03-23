41c147a9f685312b6c0dde5009885ba0634fffbf
Sakon Plangklang <iamsabell@gmail.com>
2023-03-23 15:18:45 +0700

    Create student-grading program

---
+++ day_005/travel-log.py
@@ -0,0 +1,13 @@
+travel_log = [
+  {
+    "country": "France",
+    "visits": 12,
+    "cities": ["Paris", "Lille", "Dijon"],
+  },
+  {
+    "country": "Germany",
+    "visits": 5,
+    "cities": ["Berlin", "Hamburg", "Stuttgart"]
+  },
+]
+
