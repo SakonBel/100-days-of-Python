5f103814f1e8981cafca240de4c121f715dfc24d
Sakon Plangklang <iamsabell@gmail.com>
2023-03-27 23:04:43 +0700

    Learning about how to create class, self keyword and methods

--- day_008/main.py
+++ day_008/main.py
@@ -18,3 +18,35 @@ table.add_column("Pokemon name", ["Pikachu", "Squirtle", "Charmander"])
 table.add_column("Type", ["Electric", "Water", "Fire"])
 table.align = "l"
 print(table)
+
+
+# class User:
+#     pass
+
+
+# user_1 = User()
+# user_1.id = "001"
+# user_1.username = "bel"
+
+# print(user_1.username)
+
+class User:
+    def __init__(self, user_id, name):
+        self.id = user_id
+        self.username = name
+        self.followers = 0
+        self.followings = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.followings += 1
+
+
+user_1 = User("001", "bel")
+user_2 = User("002", "maew")
+
+user_1.follow(user_2)
+print(user_1.followers)
+print(user_1.followings)
+print(user_2.followers)
+print(user_2.followings)
