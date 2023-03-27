3712cc6cb18c3cc1642537b9b97487d893742362
Sakon Plangklang <iamsabell@gmail.com>
2023-03-27 23:17:01 +0700

    Create question-data and question model

---
+++ day_008/quiz-game/question-data.py
@@ -0,0 +1,6 @@
+question_data = [
+    {"text": "A slug'blood is green.", "answer": "True"},
+    {"text": "The loudest animal is the African elephant.", "answer": "False"},
+    {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
+    {"text": "The total surface area of human lungs is the size of a football pitch.", "answer": "True"},
+]
