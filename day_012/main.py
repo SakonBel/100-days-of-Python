360cecb92165545590068a0fb230c51f37e3b491
Sakon Plangklang <iamsabell@gmail.com>
2023-03-31 22:59:30 +0700

    Create scoreboard and how to count the score

--- day_012/main.py
+++ day_012/main.py
@@ -2,6 +2,7 @@ from turtle import Turtle, Screen
 from time import sleep
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -12,6 +13,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -28,5 +30,6 @@ while game_is_on:
 
     if snake.head.distance(food) < 15:
         food.refresh()
+        scoreboard.increase_score()
 
 screen.exitonclick()
