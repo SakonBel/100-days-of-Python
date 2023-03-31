a5e83d42691211d42d3e24ef027be57c24ff3a8c
Sakon Plangklang <iamsabell@gmail.com>
2023-04-01 02:26:20 +0700

    Finished Pong Game

--- day_014/pong/main.py
+++ day_014/pong/main.py
@@ -2,6 +2,7 @@ from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
 from time import sleep
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.bgcolor("black")
@@ -13,6 +14,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 
 screen.listen()
@@ -23,7 +25,7 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    sleep(0.05)
+    sleep(ball.move_speed)
     screen.update()
     ball.move()
 
@@ -32,5 +34,13 @@ while game_is_on:
 
     if (ball.distance(r_paddle) < 50 and ball.xcor() > 340) or (ball.distance(l_paddle) < 50 and ball.xcor() < -340):
         ball.bounce_x()
+
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
